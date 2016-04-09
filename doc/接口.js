949ba4322b75a24f90603aaf009166edf51bab86
MaChi <loyuk@sina.com>
2016-04-09 23:56:24 +0800

    update
    update

---
+++ doc/接口.js
@@ -0,0 +1,31 @@
+//图1
+
+// ---- 10.0.0.1:80/xxx?category=recommend&step=1
+{   
+    //广告列表
+    adv:[ 
+        {
+            imageUrl:'',
+            imageTitle:''
+        }, {
+            imageUrl:'',
+            imageTitle:''
+        }
+        //......
+    ],
+
+    //产品列表
+    productList:[
+        {
+            imageUrl:'',
+            imageTitle:'',
+            productID:''
+        },
+        {
+            imageUrl:'',
+            imageTitle:'',
+            productID:''
+        }
+        //......
+    ]
+}
